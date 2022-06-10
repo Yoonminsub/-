ef58c962621a816239af0c69bac94fcb2bc048a8
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-10 18:52:52 +0900

    Create B1644.cpp

---
+++ 소수판정(에라토스테네스의 체)/B1644.cpp
@@ -0,0 +1,52 @@
+/*
+* minsub Yoon
+* 2022.3.19
+*https://www.acmicpc.net/problem/1644
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+int A[4000005];
+vector<int> S;
+int main(){
+    scanf("%d",&n);
+    int i;
+    A[0]=1;
+    A[1]=1;
+    for(i=2;i<=n;i++){
+        if(A[i]==0){
+            int j;
+            S.push_back(i);
+            for(j=2;j<=n/i;j++){
+                A[j*i]=1;
+            }
+        }
+    }
+    int t=S.size();
+    int f=0;
+    int l=0;
+    int s;
+    if(!S.empty()){
+        s=S[0];
+    }
+    int m=0;
+    while(1){
+        //printf("%d ",s);
+        if(l==t){
+            break;
+        }
+        if(s>=n){
+            if(s==n){
+                m++;
+            }
+            s=s-S[f];
+            f++;
+        }else{
+            l++;
+            s=s+S[l];
+        }
+    }
+    printf("%d",m);
+    return 0;
+}
