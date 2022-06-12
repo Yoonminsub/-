c1080140ce0268482b902d9837e3ce89c584613f
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-12 23:34:06 +0900

    Create B1931.cpp

---
+++ 정렬 알고리즘/B1931.cpp
@@ -0,0 +1,37 @@
+/*
+*
+*
+*
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<pair<int,int>> A;
+int n;
+int main(){
+    scanf("%d",&n);
+    int a,b;
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        A.push_back({a,b});
+    }
+    sort(A.begin(),A.end());
+    int t=1;
+    a=A[0].first;
+    b=A[0].second;
+    for(i=1;i<n;i++){
+        if(A[i].second<b){
+            a=A[i].first;
+            b=A[i].second;
+        }else if(A[i].first>=b){
+            t++;
+            a=A[i].first;
+            b=A[i].second;
+        }
+        
+    }
+    printf("%d",t);
+    return 0;
+}
