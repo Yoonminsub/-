6226e66434c9ed7507f92ac284ee564f812015a2
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-14 23:17:30 +0900

    Create B1026.cpp

---
+++ 정렬 알고리즘/B1026.cpp
@@ -0,0 +1,34 @@
+/*
+*
+*2022.1.14
+*https://www.acmicpc.net/problem/1026
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<int> A;
+vector<int> B;
+int n;
+int main(){
+    int j;
+    scanf("%d",&n);
+    for(j=0;j<n;j++){
+        int t;
+        cin >> t;
+        A.push_back(t);
+    }
+    for(j=0;j<n;j++){
+        int t;
+        cin >> t;
+        B.push_back(t);
+    }
+    sort(A.begin(),A.end());
+    sort(B.rbegin(),B.rend());
+    int a=0;
+    for(j=0;j<n;j++){
+        a=a+B[j]*A[j];
+    }
+    printf("%d",a);
+    return 0;
+}
