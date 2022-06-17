a02f1e5b3be17f7872493f7408cb5b5545d65a5e
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-17 23:18:29 +0900

    Create B11728.cpp

---
+++ 정렬 알고리즘/B11728.cpp
@@ -0,0 +1,45 @@
+/*
+*
+*2022.3.25
+*https://www.acmicpc.net/problem/11728
+*/
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int n,m;
+int A[1000005];
+int B[1000005];
+int main(){
+    scanf("%d %d",&n,&m);
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&A[i]);
+    }
+    for(i=0;i<m;i++){
+        scanf("%d",&B[i]);
+    }
+    sort(A,A+n);
+    sort(B,B+m);
+    int a=0;
+    int b=0;
+    while(1){
+        if(a==n||b==m){
+            break;
+        }
+        if(A[a]>B[b]){
+            printf("%d ",B[b]);
+            b++;
+        }else{
+            printf("%d ",A[a]);
+            a++;
+        }
+    }
+    for(i=a;i<n;i++){
+        printf("%d ",A[i]);
+    }
+    for(i=b;i<m;i++){
+        printf("%d ",B[i]);
+    }
+    return 0;
+}
