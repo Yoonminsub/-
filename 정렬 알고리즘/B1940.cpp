b4a730c76916fb421b537610c5243d68ff6ab281
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-18 23:22:24 +0900

    Create B1940.cpp

---
+++ 정렬 알고리즘/B1940.cpp
@@ -0,0 +1,43 @@
+/*
+*
+*2022.3.18
+*https://www.acmicpc.net/problem/1940
+*/
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+int A[15005];
+int n,m;
+int main(){
+    scanf("%d %d",&n,&m);
+    int i;
+    for(i=0;i<n;i++){
+        int a;
+        scanf("%d",&a);
+        A[i]=a;
+    }
+    sort(A,A+n);
+    int f=0;
+    int l=n-1;
+    int t=0;
+    while(1){
+        if(f>=l){
+            break;
+        }
+        if(A[f]+A[l]>m){
+            l--;
+        }else{
+            if(A[f]+A[l]==m){
+                t++;
+                f++;
+                l=n-1;
+            }else{
+                f++;
+                l=n-1;
+            }
+        }
+    }
+    printf("%d",t);
+    return 0;
+}
