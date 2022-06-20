a70408c2e381b1122b9a2fab0f4d17c5f0253b71
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-20 23:12:25 +0900

    Create  B2548.cpp

---
+++ 정렬 알고리즘/B2548.cpp
@@ -0,0 +1,44 @@
+/*
+*
+*2022.3.2
+*https://www.acmicpc.net/problem/2548
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int A[500005];
+int B[500005];
+int n;
+int output(int a){
+    int i=1;
+    int j=n;
+    int s;
+    // i : 5, j : 5, s : 5, a : 5
+    // A : 1 2 3 4 5
+    while(i<=j){
+        s=(i+j)/2;
+        //printf("%d %d ",i,j);
+        if(A[s]>a){ // 4 > 5
+            j=s-1;
+        }else if(A[s] == a){
+            return 1;
+        }
+        else{
+            i=s+1;
+        }
+    }
+    return 0;
+}
+int main(){
+    scanf("%d",&n);
+    int i;
+    for(i=1;i<=n;i++){
+        int a;
+        scanf("%d",&a);
+        A[i]=a;
+    }
+    sort(A+1,A+n+1);
+    int t=(1+n)/2;
+    printf("%d",A[t]);
+    return 0;
+}
