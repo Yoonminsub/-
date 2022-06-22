73e338022657c888b159eec52d9ebe30aaa39832
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-22 23:14:09 +0900

    Create B23559.cpp

---
+++ 정렬 알고리즘/B23559.cpp
@@ -0,0 +1,42 @@
+/*
+*
+*2022.1.21
+*https://www.acmicpc.net/problem/23559
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<pair<int,int>> A;
+vector<pair<int,int>> B;
+int n,m;
+int main(){
+    scanf("%d %d",&n,&m);
+    int i;
+    int x,y; //y=5000 x=1000
+    x=m-1000*n;
+    y=x/4000;
+    x=n-y;
+    int t=0;
+    for(i=0;i<n;i++){
+        int a,b;
+        scanf("%d %d",&a,&b);
+        A.push_back({b,a});
+        B.push_back({a-b,i});
+    }
+    sort(B.rbegin(),B.rend());
+    for(i=0;i<y;i++){
+        if(B[i].first<=0){
+            x++;
+        }else{
+            t+=A[B[i].second].second;
+            A[B[i].second].first=0;
+        }
+    }
+    sort(A.rbegin(),A.rend());
+    for(i=0;i<x;i++){
+        t+=A[i].first;
+    }
+    printf("%d",t);
+    return 0;
+}
