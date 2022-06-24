757c96de1f4df0bc81b1ff0bcdd598fae14e1087
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-24 17:08:28 +0900

    Create B17608.cpp

---
+++ 스택/B17608.cpp
@@ -0,0 +1,26 @@
+/*
+*
+*2020.9.18
+*https://www.acmicpc.net/problem/17608
+*/
+#include <stdio.h>
+int n;
+int A[100005];
+int main(){
+    scanf("%d",&n);
+    int i;
+    for(i=1;i<=n;i++){
+        scanf("%d",&A[i]);
+    }
+    int t;
+    t=A[n];
+    int a=1;
+    for(i=n-1;i>0;i--){
+        if(t<A[i]){
+            t=A[i];
+            a++;
+        }
+    }
+    printf("%d",a);
+    return 0;
+}
