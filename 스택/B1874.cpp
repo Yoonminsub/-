c405396d2242bb8f6ae1d4152f719d078ae54bc8
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-06-24 17:04:04 +0900

    Create B1874.cpp

---
+++ 스택/B1874.cpp
@@ -0,0 +1,51 @@
+/*
+*
+*2022.5.27
+*https://www.acmicpc.net/problem/1874
+*/
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector <int> B;
+string C[200005];
+int n;
+int a,b,c;
+int A[100005];
+int main(){
+    scanf("%d",&n);
+    int i;
+    for(i=1;i<=n;i++){
+        scanf("%d",&A[i]);
+    }
+    a=0;
+    int b=0;
+    for(i=1;i<=n;i++){
+        if(a<A[i]){
+            int t=a;
+            for(int j=0;j<A[i]-t;j++){
+                a++;
+                C[b]='+';
+                B.push_back(a);
+                b++;
+            }
+        }
+        if(a>=A[i]){
+            if(B.back()==A[i]){
+                B.pop_back();
+                C[b]='-';
+                b++;
+            }else{
+                printf("NO");
+                return 0;
+            }
+        }
+    }
+    for(int i=0; i<b; i++){
+        cout << C[i] <<"\n";
+    }
+
+    return 0;
+}
