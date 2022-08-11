dc9d2d7e30f488df0d94458e2ebb2fdcb8a8dc83
Yoonminsub <107245666+Yoonminsub@users.noreply.github.com>
2022-08-11 23:14:31 +0900

    Create B23253.cpp

---
+++ 구현 알고리즘/B23253.cpp
@@ -0,0 +1,31 @@
+/*
+*
+*2022.4.30
+*https://www.acmicpc.net/problem/23253
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+int n,m;
+int main(){
+    scanf("%d %d",&n,&m);
+    int i;
+    for(i=0;i<m;i++){
+        int s;
+        int t=200001;
+        scanf("%d",&s);
+        for(int j=0;j<s;j++){
+            int a;
+            scanf("%d",&a);
+            if(t>a){
+                t=a;
+            }else{
+                printf("No");
+                return 0;
+            }
+        }
+    }
+    printf("Yes");
+    return 0;
+}
